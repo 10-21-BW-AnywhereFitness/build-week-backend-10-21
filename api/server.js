064e78013b4847476cff82cd19fd088bec282cf0
Christina Yun <poncheeyun@yahoo.com>
2021-10-16 13:38:06 -0700

    Starting to flesh out the routers now

--- api/server.js
+++ api/server.js
@@ -1,29 +1,44 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
-const db = require('./data/db-config')
 
-function getAllUsers() { return db('users') }
+const authRouter = require('./auth/authRouter');
+const clientRouter = require('./client/clientRouter');
+const instructorRouter = require('./instructor/instructorRouter');
+// const db = require('./data/db-config')
 
-async function insertUser(user) {
-  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-  // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
-  const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
-  return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-}
+// function getAllUsers() { return db('users') }
+
+// async function insertUser(user) {
+//   // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
+//   // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
+//   // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
+//   const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
+//   return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
+// }
 
 const server = express()
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
-server.get('/api/users', async (req, res) => {
-  res.json(await getAllUsers())
+server.use('/api/auth', authRouter);
+server.use('/api/client', clientRouter);
+server.use('/api/instructor', instructorRouter);
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+  })
 })
 
-server.post('/api/users', async (req, res) => {
-  res.status(201).json(await insertUser(req.body))
-})
+
+// server.get('/api/users', async (req, res) => {
+//   res.json(await getAllUsers())
+// })
+
+// server.post('/api/users', async (req, res) => {
+//   res.status(201).json(await insertUser(req.body))
+// })
 
 module.exports = server
